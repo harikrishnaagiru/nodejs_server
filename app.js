0d3a2c8767eb9922616a05d620c0c95ad1cbdad3
harikrishna <118521049+harikrishnaagiru@users.noreply.github.com>
2024-09-14 17:10:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'root',
-    database: 'Doctor_Appointment'
+    password: 'aaaa@1234',
+    database: 'doctor_appointment'
 });
 
 app.use(express.json());
